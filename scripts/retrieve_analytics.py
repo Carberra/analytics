e98001e0466786fa6d6a4b3a4a66ac002ecf24ef
Ethan Henderson <parafoxia@carberra.xyz>
2021-01-17 23:23:16 +0000

    Added argument parsing (#2)
    You can now pass:
    - A filename using -f or --filename
    - A start date using -s or --start-date
    - An end date using -e or --end-date
    - A list of metrics using -m or --metrics

--- scripts/retrieve_analytics.py
+++ scripts/retrieve_analytics.py
@@ -1,3 +1,5 @@
+import argparse
+import datetime as dt
 import json
 import os
 from pathlib import Path
@@ -58,41 +60,101 @@ METRICS = (
 )
 
 
+class InvalidArgument(Exception):
+    pass
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Get YouTube analytics reports.",
+        epilog=(
+            "You need a Google Developers project with the YouTube Analytics API enabled to run this. "
+            "If you don't have that, you can find instructions at https://github.com/Carberra/analytics."
+        ),
+    )
+    parser.add_argument(
+        "-f",
+        "--filename",
+        default=f"analytics-{dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.csv",
+        help="The filename for the report. Defaults to 'analytics-YYYY-MM-DD-HH-MM-SS.csv'.",
+    )
+    parser.add_argument(
+        "-s",
+        "--start-date",
+        default="2005-02-14",
+        help="The start date for the report in YYYY-MM-DD format. Defaults to 2005-02-14.",
+    )
+    parser.add_argument(
+        "-e",
+        "--end-date",
+        default=dt.date.today().strftime("%Y-%m-%d"),
+        help="The start date for the report in YYYY-MM-DD format. Defaults to today.",
+    )
+    parser.add_argument(
+        "-m",
+        "--metrics",
+        default=",".join(METRICS),
+        help="A comma-seperated list of metrics to use. View the source code for a list of available metrics. Defaults to all.",
+    )
+    args = parser.parse_args()
+
+    if not args.filename.endswith((".json", ".csv")):
+        raise InvalidArgument("You can only save the report as a JSON or a CSV.")
+
+    args.filetype = args.filename.split(".")[-1]
+
+    lowest = dt.datetime(2005, 2, 14).date()
+    highest = dt.date.today()
+    sd = dt.datetime.strptime(args.start_date, "%Y-%m-%d").date()
+    ed = dt.datetime.strptime(args.end_date, "%Y-%m-%d").date()
+
+    if sd < lowest or ed < lowest:
+        raise InvalidArgument("You cannot set a date before 14 Feb 2005.")
+
+    if sd > dt.date.today() or ed > dt.date.today():
+        raise InvalidArgument("You cannot set a date in the future.")
+
+    if any(f := list(filter(lambda m: m not in METRICS, args.metrics.split(",")))):
+        raise InvalidArgument(f"One or more invalid metrics were passed: {','.join(f)}.")
+
+    return args
+
+
 def get_service():
     flow = InstalledAppFlow.from_client_secrets_file(SECRET_FILE, SCOPES)
     credentials = flow.run_console()
     return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
 
-def get_analytics(service, save_func, filename="analytics", **kwargs):
+def get_analytics(service, save_func, **kwargs):
     response = service.reports().query(**kwargs).execute()
-    save_func(response, filename)
+    save_func(response)
 
 
-def as_json(response, filename):
-    with open(DATA_PATH / f"{filename}.json", "w", encoding="utf-8") as f:
+def as_json(response):
+    with open(DATA_PATH / args.filename, "w", encoding="utf-8") as f:
         json.dump(response, f, indent=4, ensure_ascii=False)
 
 
-def as_csv(response, filename):
+def as_csv(response):
     df = pd.DataFrame()
     df = df.append(response["rows"])
     df.columns = [c["name"] for c in response["columnHeaders"]]
-    df.to_csv(DATA_PATH / f"{filename}.csv")
+    df.to_csv(DATA_PATH / args.filename)
 
 
 if __name__ == "__main__":
     os.environ["OAUTH_INSECURE_TRANSPORT"] = "1"
 
+    args = get_args()
     service = get_service()
     get_analytics(
         service,
-        as_csv,
-        filename="example_carberra_2020",
+        {"csv": as_csv, "json": as_json}[args.filetype],
         ids="channel==MINE",
-        startDate="2020-01-01",
-        endDate="2020-12-31",
-        metrics=",".join(METRICS),
+        startDate=args.start_date,
+        endDate=args.end_date,
+        metrics=args.metrics,
         dimensions="day",
         sort="day",
     )
